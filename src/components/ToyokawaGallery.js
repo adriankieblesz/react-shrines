2b81b71cfc5a09c8212c3f64004b650e70a3c592
Adrian Kieblesz <adriano.kieblesz@gmail.com>
2020-02-01 17:25:54 +0100

    Additional features for Toyokawa gallery

--- src/components/ToyokawaGallery.js
+++ src/components/ToyokawaGallery.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import './ToyokawaGallery.scss';
+import ModalWindow from './ModalWindow';
 class ToyokawaGallery extends Component {
     state = {
-        images: []
+        images: [],
+        openModal: false,
+        url: "",
+        currentElement: null
     }
     loadImages = () => {
         let images = [];
         for (let i = 0; i < 10; i++) {
-            images.push(<img src={require(`../images/Toyokawa_Inari_Temple/${i + 1}c.jpg`)} alt="toyokawa" />);
+            images.push(<img
+                src={require(`../images/Toyokawa_Inari_Temple/${i + 1}c.jpg`)} alt="toyokawa"
+                onClick={this.handleModalClick(require(`../images/Toyokawa_Inari_Temple/${i + 1}c.jpg`), (i + 1))}
+            />);
         }
 
         return images;
+    }
+    handleModalClose = () => {
+        this.setState(() => ({
+            openModal: false
+        }))
+    }
+    handleModalClick = (link, element) => {
+        this.setState(() => ({
+            openModal: true,
+            url: link,
+            currentElement: element
+        }))
     }
     componentDidMount() {
         let images = this.loadImages();
@@ -23,6 +42,13 @@ class ToyokawaGallery extends Component {
                 <div className="toyokawa-gallery-wrapper">
                     {images}
                 </div>
+                {this.state.openModal && <ModalWindow
+                    modalSource={this.state.url}
+                    closeclick={this.handleModalClose}
+                    open={this.state.openModal}
+                    slideLeft={this.handleSlideLeftImage}
+                    slideRight={this.handleSlideRightImage}
+                />}
             </div>
         );
     }
