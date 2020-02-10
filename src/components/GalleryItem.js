7a7de7a06574d62b6b35122eb524892db1167e79
Adrian Kieblesz <adriano.kieblesz@gmail.com>
2020-02-10 14:22:51 +0100

    Image optimalization

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 const GalleryItem = (props) => {
     return (
         <figure className="sensoji-gallery-item">
-            <img src={props.src} alt="" />
+            <img src={props.src} alt="" srcSet={props.srcSet} sizes={props.sizes} />
             <div onClick={props.click} className="expand-gallery-item"></div>
         </figure>
     );
