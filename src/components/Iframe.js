4d85181245f2d980b5c5a2c78f0ee752d44d1666
Adrian Kieblesz <adriano.kieblesz@gmail.com>
2020-02-19 13:03:47 +0100

    Image optimalization

--- src/components/Iframe.js
+++ src/components/Iframe.js
@@ -1,8 +1,39 @@
-import React from 'react';
-const Iframe = (props) => {
-    return (
-        <iframe className="map" title="map" src={props.source} frameBorder="0" style={{ "border": 0 }} allowFullScreen=""></iframe>
-    );
+
+import React, { Component } from 'react';
+class Iframe extends Component {
+    state = {
+        source: "",
+        load: false
+    }
+    loadIframe = (source) => {
+        return new Promise((resolve, reject) => {
+            resolve(source);
+        })
+    }
+    handleScroll = () => {
+        if (!this.state.load) {
+            this.loadIframe(this.props.source)
+                .then(respond => this.setState(() => ({ source: respond })));
+        }
+    }
+    componentDidMount() {
+
+        // this.loadIframe(this.props.source)
+        //     .then(respond => this.setState(() => ({ source: respond })));
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    render() {
+        return (
+            <iframe className="map" title="map" src={this.state.source} frameBorder="0" style={{ "border": 0 }} allowFullScreen=""></iframe>
+        );
+    }
 }
 
 export default Iframe;
+// const Iframe = (props) => {
+//     return (
+//         <iframe className="map" title="map" src={props.source} frameBorder="0" style={{ "border": 0 }} allowFullScreen=""></iframe>
+//     );
+// }
+
+// export default Iframe;
