820fb3f6da0a99dd8cbc723f145a08a4a49004af
Adrian Kieblesz <adriano.kieblesz@gmail.com>
2020-02-25 13:23:44 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-shrines/precache-manifest.1823e310d497428b7518aa45eac27439.js"
+  "/react-shrines/precache-manifest.2295747f32501505e8d858eb90bd8dee.js"
 );
 
 workbox.clientsClaim();
